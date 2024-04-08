72f9a223b0b48b2df3a914c110e7dd0b2803ec84
Jono Cooper <jono@jonocooper.com>
2024-04-08 19:51:31 +1200

    feat: load regions in the geolocation of the user

--- apps/web/src/routes/api/regions/+server.ts
+++ apps/web/src/routes/api/regions/+server.ts
@@ -1,11 +1,18 @@
-import { json, type RequestHandler } from '@sveltejs/kit'
+import { type RequestHandler, json } from '@sveltejs/kit'
 
 import { variables } from '$lib/variables'
 
+type NetlifyContext = {
+  geo: {
+    latitude: number
+    longitude: number
+  }
+}
+
 export const GET: RequestHandler = async ({ fetch, platform }) => {
-  const data = await fetch(variables.gtfsEndpoint + '/regions').then(r => r.json())
+  const data = await fetch(variables.gtfsEndpoint + '/regions').then((r) => r.json())
 
-  const netlifyPlatform = (platform as { context: unknown })
+  const netlifyPlatform = platform as { context: NetlifyContext } | undefined
 
-  return json({ ...data, userLocation: netlifyPlatform || {} })
+  return json({ ...data, userLocation: netlifyPlatform?.context.geo || {} })
 }
