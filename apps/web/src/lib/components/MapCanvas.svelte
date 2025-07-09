f8ebc99df7a271304003ca7db9ee1541a275a1e0
Jono Cooper <jono@jonocooper.com>
2025-07-09 23:40:54 +0900

    feat: add map

---
+++ apps/web/src/lib/components/MapCanvas.svelte
@@ -0,0 +1,22 @@
+<script>
+  import maplibregl from 'maplibre-gl'
+  import 'maplibre-gl/dist/maplibre-gl.css'
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    new maplibregl.Map({
+      container: 'maplibre-canvas',
+      style: 'http://localhost:8090/styles/basic-preview/style.json',
+      center: [174.764, -36.851],
+      zoom: 14,
+    })
+  })
+</script>
+
+<div id="maplibre-canvas"></div>
+
+<style>
+  #maplibre-canvas {
+    height: 100%;
+  }
+</style>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import MapCanvas from '$lib/components/MapCanvas.svelte'
+
   import '../css/app.css'
   import '../css/fonts.css'
 
@@ -9,4 +11,28 @@
   let { children }: Props = $props()
 </script>
 
-{@render children?.()}
+<main>
+  <section class="content">
+    {@render children?.()}
+  </section>
+  <section class="map">
+    <MapCanvas />
+  </section>
+</main>
+
+<style>
+  main {
+    display: flex;
+    height: 100vh;
+  }
+  .content {
+    width: 360px;
+    border-right: 1px solid var(--surface-border);
+    box-shadow: var(--surface-shadow);
+    overflow-y: scroll;
+    height: 100%;
+  }
+  .map {
+    flex: 1;
+  }
+</style>
