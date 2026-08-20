d50e69c4c76cdb9bc9715a3378a8fb69119c8f53
Jono Cooper <jono@jonocooper.com>
2026-08-20 22:50:19 +1200

    feat: pull everything as protobuf

--- apps/realtime/src/lib/realtime.ts
+++ apps/realtime/src/lib/realtime.ts
@@ -1,3 +1,4 @@
+import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
 import { logger } from '@lib/logger'
 
 import type { RealtimeRegionConfig } from '$lib/configManager'
@@ -17,33 +18,55 @@ const cache: RealtimeCache = {
   regions: {},
 }
 
-const getData = async <Type>(url?: string, headers?: Record<string, string>) => {
-  if (!url) return {} as Type
+const getData = async (url?: string, headers?: Record<string, string>) => {
+  if (!url) return []
   const res = await fetch(url, {
-    headers: headers,
+    headers: {
+      Accept: 'application/x-protobuf',
+      ...(headers || {}),
+    },
   })
   if (!res.ok) {
     logger.info(`received http ${res.status} from upstream`)
     throw new Error(`http: ${res.status}`)
   }
-  // todo: will need to support protobuf
-  const data = await res.json()
-  return data as Type
+  const buffer = await res.arrayBuffer()
+  const data = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(buffer))
+  return data.entity
 }
 
 const pullRealtimeData = (regionId: string, region: RealtimeRegionConfig) => async () => {
-  const [serviceAlerts, tripUpdates, vehicleLocations] = await Promise.all([
-    getData(region.gtfsRtServiceAlertsUrl, region.gtfsRtHeaders),
-    getData(region.gtfsRtTripUpdatesUrl, region.gtfsRtHeaders),
-    getData(region.gtfsRtVehicleLocationsUrl, region.gtfsRtHeaders),
-  ])
+  const timeStart = new Date().getTime()
+  const gtfsRtUpdates = (
+    await Promise.all([
+      getData(region.gtfsRtServiceAlertsUrl, region.gtfsRtHeaders),
+      getData(region.gtfsRtTripUpdatesUrl, region.gtfsRtHeaders),
+      getData(region.gtfsRtVehicleLocationsUrl, region.gtfsRtHeaders),
+    ])
+  ).flat()
+
+  const serviceAlerts: GtfsRealtimeBindings.transit_realtime.IAlert[] = []
+  const tripUpdates: GtfsRealtimeBindings.transit_realtime.ITripUpdate[] = []
+  const vehicleLocations: GtfsRealtimeBindings.transit_realtime.IVehiclePosition[] = []
+  gtfsRtUpdates.forEach((i) => {
+    if (i.alert) {
+      serviceAlerts.push(i.alert)
+    }
+    if (i.tripUpdate) {
+      tripUpdates.push(i.tripUpdate)
+    }
+    if (i.vehicle) {
+      vehicleLocations.push(i.vehicle)
+    }
+  })
   cache.regions[regionId] = {
     lastUpdated: new Date(),
     serviceAlerts,
     tripUpdates,
     vehicleLocations,
   }
-  logger.info('rt update ' + regionId)
+  const elapsedTime = new Date().getTime() - timeStart
+  logger.info(`${regionId} rt update (${elapsedTime}ms)`)
 }
 
 export const startRealtime = (regions: (RealtimeRegionConfig & { id: string })[]) => {
