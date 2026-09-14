05d33b5f7f5b7480b8294e95173d56789b2a6068
Jono Cooper <jono@jonocooper.com>
2026-09-15 07:49:30 +1200

    chore: add error page

---
+++ apps/web/src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { page } from '$app/state'
+  import Header from '$lib/components/Header.svelte'
+  import ScrollContainer from '$lib/components/ScrollContainer.svelte'
+</script>
+
+<Header title="Error" />
+<ScrollContainer>
+  <div>
+    <p>{page.error?.message}</p>
+    <p><small>Error code: {page.status}</small></p>
+  </div>
+</ScrollContainer>
+
+<style>
+  div {
+    padding: var(--edge-padding);
+    text-align: center;
+  }
+  small {
+    color: var(--surface-text-subtle);
+  }
+</style>
